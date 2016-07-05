187e0e3ce8d06fffa19705be7259ff216f1e3378
karengianne <karen.lemaf@gmail.com>
2016-07-05 17:42:17 -0300

    primeiro commit

---
+++ src/A.java
@@ -0,0 +1,32 @@
+
+public class A {
+	
+	private int A1;
+	
+	private int A2;
+
+	public int getA1() {
+		return A1;
+	}
+
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+
+	public int getA2() {
+		return A2;
+	}
+
+	public void setA2(int a2) {
+		A2 = a2;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
